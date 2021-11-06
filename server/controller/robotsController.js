8ad812a7f1b6c1cc9e74f80f2629eb1c45a9deb2
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-06 19:30:33 +0100

    :memo: Adding GetById GET method

--- server/controller/robotsController.js
+++ server/controller/robotsController.js
@@ -12,4 +12,25 @@ const getRobots = async (req, res) => {
   res.json(robots);
 };
 
-module.exports = getRobots;
+const getRobotById = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const searchedRobot = await Robot.findById(id);
+    if (searchedRobot) {
+      debug(chalk.bgBlue.white("₍ᐢ.⚇.ᐢ₎ GET a una ID contra BBDD OK ₍ᐢ.⚇.ᐢ₎"));
+      res.json(searchedRobot);
+    } else {
+      debug(chalk.bgYellow.black("₍•̀⚇•́₎ GET a una ID contra BBDD KO ₍•̀⚇•́₎"));
+      const error = new Error(
+        "The requested robot it's not fabricated yet  ʅ(°,ʖ°)ʃ"
+      );
+      error.code = 404;
+      throw error;
+    }
+  } catch (error) {
+    error.code = 400;
+    next(error);
+  }
+};
+
+module.exports = { getRobots, getRobotById };
